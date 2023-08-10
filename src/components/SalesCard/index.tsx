99f2f1bd45ac5bd597e4c98f12b76be81b9cad6f
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-10 18:00:35 +0100

    React Hook useState

--- src/components/SalesCard/index.tsx
+++ src/components/SalesCard/index.tsx
@@ -1,10 +1,17 @@
 
+import { useState } from 'react';
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 function SalesCard() {
+
+  const max = new Date();
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate,setMaxDate] = useState(max);
 
   return (
     <>
@@ -13,16 +20,16 @@ function SalesCard() {
         <div>
           <div className="dsmeta-form-control-container">
             <DatePicker
-              selected={new Date()}
-              onChange={(date: Date) => { }}
+              selected={minDate}
+              onChange={(date: Date) => setMinDate(date)}
               className="dsmeta-form-control"
               dateFormat="dd/MM/yyyy"
             />
           </div>
           <div className="dsmeta-form-control-container">
             <DatePicker
-              selected={new Date()}
-              onChange={(date: Date) => { }}
+              selected={maxDate}
+              onChange={(date: Date) => setMaxDate(date)}
               className="dsmeta-form-control"
               dateFormat="dd/MM/yyyy"
             />
