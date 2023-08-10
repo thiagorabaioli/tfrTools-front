3ad6c6510f86bc1ba7364e39d0bd2de44916fa5e
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-10 15:49:11 +0100

    First component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 
 function App() {
  
   return (
+    <>
     <h1> Ola Mundo</h1>
+    <NotificationButton />
+    </>
   )
 }
 
