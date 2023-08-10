649f2404386782ea961838b127a41fb71b3ca767
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-10 17:36:58 +0100

    Other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+
 
 
 function App() {
@@ -7,8 +8,13 @@ function App() {
   return (
     <>
     <Header />
-    <h1> Ola Mundo</h1>
-    <NotificationButton />
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container"> 
+        <SalesCard />
+        </div>
+        </section>
+        </main>
     </>
   )
 }
