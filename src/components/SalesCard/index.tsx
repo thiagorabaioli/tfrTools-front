ff546f1c4a494f86cf1c0a7bd386746e5466b57b
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-11 11:28:18 +0100

    axios request

--- src/components/SalesCard/index.tsx
+++ src/components/SalesCard/index.tsx
@@ -1,9 +1,10 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 function SalesCard() {
 
@@ -12,6 +13,12 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate,setMaxDate] = useState(max);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/categorias/1").then(response => {
+      console.log(response.data);
+    })
+  }, []);
 
   return (
     <>
